b0fc656b327cf790819418e4b74972cfe1b41d95
jinxudong <18751241086@163.com>
2022-03-26 23:46:25 +0800

    首页部分组件开发

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,12 +1,27 @@
-import React, {Component} from "react";
-import { HerderWrapper,Logo,Nav } from "./style";
+import React, { Component } from "react";
+import { HerderWrapper, Logo, Nav, NavItem, Navsearch, Addition, Button, SearchWrapper } from "./style";
 
 class Header extends Component {
   render() {
     return (
       <HerderWrapper>
         <Logo></Logo>
-        <Nav></Nav>
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载APP</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <Navsearch></Navsearch>
+            <i className="iconfont">&#xe614;</i>
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className="writing">写文章</Button>
+          <Button className="reg">注册</Button>
+        </Addition>
       </HerderWrapper>
     )
   }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -8,7 +8,7 @@ export const HerderWrapper = styled.div`
 `
 
 export const Logo = styled.a.attrs({
-  href:'./'
+  href: './'
 })`
   position:absolute;
   top:0;
@@ -23,6 +23,82 @@ export const Logo = styled.a.attrs({
 export const Nav = styled.div`
   width:960px;  
   height:58px;
-  background:red;
+  padding-right:70px;
+  border-box:box-sizing;
   margin:0 auto;
+`
+
+export const NavItem = styled.div`
+  &.left{
+    float:left
+  }
+  &.right{
+    float:right
+  }
+  &.active{
+    color:#ea6f5a;
+  }
+  line-height:58px;
+  padding: 0 15px;
+  color:#333;
+  font-size:17px;
+`
+
+export const Navsearch = styled.input.attrs({
+  placeholder: '搜索'
+})`
+  width:160px;  
+  height:38px; 
+  margin-top:9px;
+  padding:0 20px;
+  margin-left:20px;
+  box-sizing:border-box;
+  outline:none;
+  border:none;
+  border-radius:19px;
+  background:#eee;
+  &::placeholder{
+    color:#999;
+  }
+`
+
+export const Addition = styled.div`
+  position:absolute;
+  right:0;
+  top:0;
+  height:56px;
+  line-hieght:56px;
+`
+export const Button = styled.div`
+  float:right;
+  margin-top:9px;
+  margin-right:20px;
+  padding:0 20px;
+  line-height:38px;
+  border-radius:19px;
+  border:1px solid #ec6149;
+  &.reg{
+    color:#ec6149;
+  }
+  &.writing{
+    color:#fff;
+    background:#ec6149;
+  }
+  :hover{
+    cursor:pointer;
+  }
+`
+export const SearchWrapper = styled.div`
+  position:relative;
+  float:left;
+  .iconfont{
+    position:absolute;
+    right:5px;
+    bottom:5px;
+    width:30px;
+    line-height:30px;
+    border-radius:15px;
+    background:green;
+    text-align:center;
+  }
 `
