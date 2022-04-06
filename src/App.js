4613779fb87349db4d8d90382a32d5b9c42f0995
jinxudong <18751241086@163.com>
2022-04-06 23:06:44 +0800

    简书项目开发

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Header from './common/header'
 import {Provider} from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Detail from './pages/detail'
 import store from './store'
 
 function App() {
   return (
     <Provider store={store}>
-      <Header/>
+      <BrowserRouter>
+      		<div>
+            <Header />
+      			<Route path='/' exact component={Home}></Route>
+            {/* <Route path='/login' exact component={Login}></Route>
+            <Route path='/write' exact component={Write}></Route> */}
+      			<Route path='/detail/:id' exact component={Detail}></Route>
+      		</div>
+      	</BrowserRouter>
     </Provider>
     
   );
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux-immutable'
 import headerReducer from '../common/header/store/reducer'
+import { reducer as homeReducer } from '../pages/home/store'
+import { reducer as detailReducer } from '../pages/detail/store';
 
 export default combineReducers({
-  header:headerReducer
+  header:headerReducer,
+  home: homeReducer,
+  detail: detailReducer,
 })
